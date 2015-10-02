a95f81f4188fb20c72f03b125c597bc517d7b084
Bart Vrancken <bart@abuse.io>
2015-10-02 20:55:21 +0200

    update layout, protect pregmatch and return warning if no content collected

--- src/Blocklistde.php
+++ src/Blocklistde.php
@@ -2,9 +2,6 @@
 
 namespace AbuseIO\Parsers;
 
-use ReflectionClass;
-use Log;
-
 class Blocklistde extends Parser
 {
     /**
@@ -13,7 +10,7 @@ class Blocklistde extends Parser
     public function __construct($parsedMail, $arfMail)
     {
         // Call the parent constructor to initialize some basics
-        parent::__construct($parsedMail, $arfMail);
+        parent::__construct($parsedMail, $arfMail, $this);
     }
 
     /**
@@ -23,55 +20,48 @@ class Blocklistde extends Parser
      */
     public function parse()
     {
-        // Generalize the local config based on the parser class name.
-        $reflect = new ReflectionClass($this);
-        $this->configBase = 'parsers.' . $reflect->getShortName();
-
-        Log::info(
-            get_class($this) . ': Received message from: ' .
-            $this->parsedMail->getHeader('from') . " with subject: '" .
-            $this->parsedMail->getHeader('subject') . "' arrived at parser: " .
-            config("{$this->configBase}.parser.name")
-        );
-
         foreach ($this->parsedMail->getAttachments() as $attachment) {
             // Only use the Blocklistde formatted reports, skip all others
             if (preg_match(config("{$this->configBase}.parser.report_file"), $attachment->filename)) {
-                preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $attachment->getContent(), $regs);
-                $report = array_combine($regs[1], $regs[2]);
+                if (preg_match_all('/([\w\-]+): (.*)[ ]*\r?\n/', $attachment->getContent(), $regs)) {
+                    $report = array_combine($regs[1], $regs[2]);
 
-                // We need this field to detect the feed, so we need to check it first
-                if (!empty($report['Report-Type'])) {
-                    $this->feedName = $report['Report-Type'];
+                    // We need this field to detect the feed, so we need to check it first
+                    if (!empty($report['Report-Type'])) {
+                        $this->feedName = $report['Report-Type'];
 
-                    // If report type is an alias, get the real type
-                    foreach (config("{$this->configBase}.parser.aliases") as $alias => $real) {
-                        if ($report['Report-Type'] == $alias) {
-                            $this->feedName = $real;
+                        // If report type is an alias, get the real type
+                        foreach (config("{$this->configBase}.parser.aliases") as $alias => $real) {
+                            if ($report['Report-Type'] == $alias) {
+                                $this->feedName = $real;
+                            }
                         }
-                    }
 
-                    // If feed is known and enabled, validate data and save report
-                    if ($this->isKnownFeed() && $this->isEnabledFeed()) {
-                        // Sanity check
-                        if ($this->hasRequiredFields($report) === true) {
-                            // Event has all requirements met, filter and add!
-                            $report = $this->applyFilters($report);
+                        // If feed is known and enabled, validate data and save report
+                        if ($this->isKnownFeed() && $this->isEnabledFeed()) {
+                            // Sanity check
+                            if ($this->hasRequiredFields($report) === true) {
+                                // Event has all requirements met, filter and add!
+                                $report = $this->applyFilters($report);
 
-                            $this->events[] = [
-                                'source'        => config("{$this->configBase}.parser.name"),
-                                'ip'            => $report['Source'],
-                                'domain'        => false,
-                                'uri'           => false,
-                                'class'         => config("{$this->configBase}.feeds.{$this->feedName}.class"),
-                                'type'          => config("{$this->configBase}.feeds.{$this->feedName}.type"),
-                                'timestamp'     => strtotime($report['Date']),
-                                'information'   => json_encode($report),
-                            ];
+                                $this->events[] = [
+                                    'source' => config("{$this->configBase}.parser.name"),
+                                    'ip' => $report['Source'],
+                                    'domain' => false,
+                                    'uri' => false,
+                                    'class' => config("{$this->configBase}.feeds.{$this->feedName}.class"),
+                                    'type' => config("{$this->configBase}.feeds.{$this->feedName}.type"),
+                                    'timestamp' => strtotime($report['Date']),
+                                    'information' => json_encode($report),
+                                ];
+                            }
                         }
+                    } else {
+                        // We cannot parse this report, since we haven't detected a report_type.
+                        $this->warningCount++;
                     }
                 } else {
-                    // We cannot parse this report, since we haven't detected a report_type.
+                    // We cannot parse this report, since we cant collect event data.
                     $this->warningCount++;
                 }
             } // end if: found report file to parse
